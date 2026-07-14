6261b3f834e2232ecd6889fae38bcb07176c14a6
itsshashanksp <9945shashank@gmail.com>
2026-07-14 13:04:20 +0530

    Create CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production"
-    ? "/sigma-website/"
-    : "/",
+  base: "/",
   build: {
     outDir: "docs",
   },
